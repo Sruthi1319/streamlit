57da55f30207124612069b6ce63b6cc52909e3ac
kashettisruthi <kashettisruthi13@gmail.com>
2025-07-19 14:42:16 +0530

    intial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title('Interactive User Interface Example')
+
+# Take text input
+user_text = st.text_input('Enter some text')
+
+# Add a slider
+slider_value = st.slider('Select a number', 0, 100, 50)
+
+# Button interaction
+if st.button('Submit'):
+    st.write(f'You entered: {user_text}')
+    st.write(f'Slider value: {slider_value}')
